25fc61f8272b5b375cd757222617c0dd851cdb09
Zitao Xiong <caoer115@gmail.com>
2015-07-21 03:29:46 -0400

    tune

--- DBCamera/Controllers/DBCameraSegueViewController.m
+++ DBCamera/Controllers/DBCameraSegueViewController.m
@@ -60,7 +60,8 @@ static const CGSize kFilterCellSize = { 75, 90 };
         [self initVignetteFilter];
         
         _cropArray = @[ @320, @213, @240, @192, @180 ];
-        _filtersList = @[ @"normal", @"1977", @"amaro", @"grey", @"hudson", @"mayfair", @"nashville", @"valencia", @"contrastgrey", @"vignette" ];
+//        _filtersList = @[ @"normal", @"1977", @"amaro", @"grey", @"hudson", @"mayfair", @"nashville", @"valencia", @"contrastgrey", @"vignette" ];
+        _filtersList = @[];
         _filterMapping = @{ @0:[[GPUImageFilter alloc] init],
                             @1:[[GPUImageToneCurveFilter alloc] initWithACV:@"1977"],
                             @2:[[GPUImageToneCurveFilter alloc] initWithACV:@"amaro"],
@@ -243,8 +244,8 @@ static const CGSize kFilterCellSize = { 75, 90 };
         [_navigationBar setUserInteractionEnabled:YES];
         [_navigationBar addSubview:self.useButton];
         [_navigationBar addSubview:self.retakeButton];
-        if ( !_forceQuadCrop )
-            [_navigationBar addSubview:self.cropButton];
+//        if ( !_forceQuadCrop )
+//            [_navigationBar addSubview:self.cropButton];
     }
     
     return _navigationBar;
